f0f56b1923aae61f5307b5c470c5b072fc8ea671
Diegoo11 <ynoacamino@gmail.com>
2023-11-27 18:21:00 -0500

    change color of button 'Compartir'

--- src/app/components/Publicaciones/ArticleBox.jsx
+++ src/app/components/Publicaciones/ArticleBox.jsx
@@ -43,9 +43,9 @@ export default function ArticleBox({
             target="_blank"
             href={`https://www.facebook.com/sharer/sharer.php?u=https://miguelsalinasjuridico.com/publicaciones/${seccion.toLowerCase()}/${path}/&src=sdkpreparse`}
           >
-            <Button color="primary">
+            <button type="button" class="z-0 group relative inline-flex items-center justify-center box-border appearance-none select-none whitespace-nowrap font-normal subpixel-antialiased overflow-hidden tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 px-unit-4 min-w-unit-20 h-unit-10 text-small gap-unit-2 rounded-medium [&>svg]:max-w-[theme(spacing.unit-8)] data-[pressed=true]:scale-[0.97] transition-transform-colors-opacity motion-reduce:transition-none bg-[#191970] text-primary-foreground data-[hover=true]:opacity-hover">
               Compartir
-            </Button>
+            </button>
           </Link>
           {pdfSrc && (
             <Link href={pdfSrc} target="_blank">
